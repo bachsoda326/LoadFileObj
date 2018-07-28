103d51d2f18582f8d61ad569629bc26fa127dab8
bachsoda326 <35907949+bachsoda326@users.noreply.github.com>
2018-07-29 03:12:15 +0700

    Load .obj

---
+++ Load File Obj/Assets/Scripts/DownloadObjToResources.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class DownloadObjToResources : MonoBehaviour
+{
+    public string url = "";
+    public string fileName = "";
+
+    public void BtnDownLoad()
+    {        
+        StartCoroutine(Download());
+    }
+
+    IEnumerator Download()
+    {
+        var client = new WebClient();
+        client.DownloadFile("http://likebyduybao.16mb.com/bmw_x5/BMWX54.obj", Application.dataPath + @"/Resources/" + fileName);
+
+        yield return client;
+
+        Debug.Log("Download Finished!");
+    }
+}
---
+++ Load File Obj/Assets/Scripts/LoadObjFromResources.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadObjFromResources : MonoBehaviour
+{
+    public string objName = "";
+
+    public void BtnLoadObj()
+    {
+        GameObject obj = Instantiate(Resources.Load(objName, typeof(GameObject))) as GameObject;
+    }
+}
